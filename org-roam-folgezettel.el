2c328aa430f71f470cd48654794045d39c98301d
Kristoffer Balintona <krisbalintona@gmail.com>
2024-11-08 11:57:43 -0600

    Create main file

---
+++ org-roam-folgezettel.el
@@ -0,0 +1,38 @@
+;;; org-roam-folgezettel.el --- Folgezettel interface for Org-roam  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Kristoffer Balintona
+
+;; Author: Kristoffer Balintona <krisbalintona@gmail.com>
+;; Keywords: files, text, convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Folgezettel interface for Org-roam.
+
+;;; Code:
+
+;;; Variables
+;;;; Options
+
+;;;; Internal
+
+;;; Functions
+
+;;; Commands
+
+;;; Provide
+(provide 'org-roam-folgezettel)
+;;; org-roam-folgezettel.el ends here
