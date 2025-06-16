c66084e48410b661b3c10611658125f4420f5766
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-16 00:29:10 -0500

    Remove non-default values for :truncate-guess

--- org-roam-folgezettel.el
+++ org-roam-folgezettel.el
@@ -64,18 +64,15 @@ like `org-roam-ql-search'accepts."
                      :primary ascend
                      :align left
                      :formatter ,#'org-roam-folgezettel--index-formatter
-                     :comparator ,#'org-roam-folgezettel--index-lessp
-                     :truncate-guess 10)
+                     :comparator ,#'org-roam-folgezettel--index-lessp)
                    ( :name "Path"
                      :align left
                      :max-width "65%"
-                     :formatter ,#'org-roam-folgezettel--path-formatter
-                     :truncate-guess 10)
+                     :formatter ,#'org-roam-folgezettel--path-formatter)
                    ( :name "Tags"
                      :align right
                      :formatter ,#'org-roam-folgezettel--tags-formatter
-                     :max-width "25%"
-                     :truncate-guess 10))
+                     :max-width "25%"))
         :use-header-line t
         :separator-width 2
         :column-color-function #'org-roam-folgezettel--column-color-function
